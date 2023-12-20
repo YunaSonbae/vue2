af41efaef3a7f83df83213f3bc520a0a027d58e6
Yuna <114555777+YunaSonbae@users.noreply.github.com>
2023-12-20 10:49:10 +0700

    add newStatus2

--- js/main.js
+++ js/main.js
@@ -106,3 +106,139 @@ Vue.component('cols', {
                 }
             }
         },
+        newStatus2(card, t) {
+            t.completed = true
+            let count = 0
+            card.status = 0
+            for (let i = 0; i < 5; i++) {
+                if (card.subtasks[i].title != null) {
+                    count++
+                }
+            }
+
+            for (let i = 0; i < count; i++) {
+                if (card.subtasks[i].completed === true) {
+                    card.status++
+                }
+            }
+            if (card.status/count*100 === 100) {
+                this.column3.push(card)
+                this.column2.splice(this.column2.indexOf(card), 1)
+                card.date = new Date()
+            }
+            if(this.column2.length < 5) {
+                if(this.column1.length > 0) {
+                    this.column1.forEach(item => {
+                        item.subtasks.forEach(item => {
+                            item.completed = false;
+                        })
+                    })
+                }
+            }
+        }
+    },
+    
+
+    computed: {
+
+    },
+    props: {
+        card: {
+            title: {
+                type: Text,
+                required: true
+            },
+            subtasks: {
+                type: Array,
+                required: true,
+                completed: {
+                    type: Boolean,
+                    required: true
+                }
+            },
+            date: {
+                type: Date,
+                required: false
+            },
+            status: {
+                type: Number,
+                required: true
+            },
+            errors: {
+                type: Array,
+                required: false
+            }
+        },
+    },
+       
+   
+})
+
+
+Vue.component('newcard', {
+    template: `
+    <form class="addform" @submit.prevent="onSubmit">
+        <p>
+            <label for="title">Title</label>
+            <input id="title" required v-model="title" type="text" placeholder="title">
+        </p>
+        <div class="subtask-wrapper">
+            <div>
+                <input required id="subtask1" v-model="subtask1" placeholder="subtask">
+            </div>
+            <div>
+                <input required id="subtask2" v-model="subtask2" maxlength="30" placeholder="subtask">
+            </div>
+            <div>
+                <input required id="subtask3" v-model="subtask3" maxlength="30" placeholder="subtask">
+            </div>
+            <div>
+                <input  id="subtask4" v-model="subtask4" maxlength="30" placeholder="subtask">
+            </div>
+            <div>
+                <input  id="subtask5" v-model="subtask5" maxlength="30" placeholder="subtask">
+            </div>
+        </div>
+        <button type="submit">Add a card</button>
+    </form>
+    `,
+    data() {
+        return {
+            title: null,
+            subtask1: null,
+            subtask2: null,
+            subtask3: null,
+            subtask4: null,
+            subtask5: null,
+            errors: [],
+        }
+    },
+    methods: {
+        onSubmit() {
+            let card = {
+                title: this.title,
+                subtasks: [{title: this.subtask1, completed: false},
+                            {title: this.subtask2, completed: false},
+                            {title: this.subtask3, completed: false},
+                            {title: this.subtask4, completed: false},
+                            {title: this.subtask5, completed: false}],
+                date: null,
+                status: 0
+            }
+            eventBus.$emit('card-submitted', card)
+            this.title = null
+            this.subtask1 = null
+            this.subtask2 = null
+            this.subtask3 = null
+            this.subtask4 = null
+            this.subtask5 = null
+        }
+    }
+})
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        name: 'Notes'
+    }
+})
